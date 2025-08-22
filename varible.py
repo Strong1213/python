42f2e4f804228407cea803b98433a01cec4fa817
Islam navi <islamnavi2727@gmail.com>
2025-08-23 00:36:40 +0530

    varible.py

---
+++ varible.py
@@ -0,0 +1,23 @@
+# Python Variables Example
+
+# Integer variable
+age = 21
+print("Age:", age)
+
+# Float variable
+price = 99.99
+print("Price:", price)
+
+# String variable
+name = "Raza"
+print("Name:", name)
+
+# Boolean variable
+is_student = True
+print("Is Student:", is_student)
+
+# Multiple variables in one line
+x, y, z = 10, 20, 30
+print("x:", x, "y:", y, "z:", z)
+
+# Single value to multip
